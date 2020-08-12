0b564a53d0ec5a29e925959d20c0ce47ae316927
aashay4123 <aashayphirke5798@gmail.com>
2020-08-12 20:10:22 +0530

    production error

--- client/src/utility.js
+++ client/src/utility.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getcookie } from "./components/helper";
 const token = getcookie("token");
-// const BASE_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:9001' : '';
+const BASE_URL = window.host ? window.host : "http://localhost:3000";
 
 export const updateObject = (oldObject, updatedProperties) => {
   return {
@@ -11,10 +11,10 @@ export const updateObject = (oldObject, updatedProperties) => {
 };
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API,
+  baseURL: `${BASE_URL}/api`,
 });
 
 export const axiosInstanceAuth = axios.create({
-  baseURL: process.env.REACT_APP_API,
+  baseURL: `${BASE_URL}/api`,
   headers: { Authorization: `Bearer ${token}` },
 });
